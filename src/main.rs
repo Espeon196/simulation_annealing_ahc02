fce591e595e8ae8f44cd0450d482b1d848f9883b
Espeon196 <z3ijnn3@gmail.com>
2025-09-13 13:15:41 +0900

    edgeを張る操作追加

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use proconio;
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
 
-const H: i64 = 50;
-const W: i64 = 50;
-const TILE_NUM: i64 = H * W;
+const H: usize = 50;
+const W: usize = 50;
+const TILE_NUM: usize = H * W;
 
 use lazy_static::lazy_static;
 
@@ -28,50 +28,52 @@ lazy_static! {
         input! {points: [i64; TILE_NUM as usize]}
         points
     };
+    static ref NEXT_COORDS: Vec<Vec<usize>> = initialize_global_info();
 }
 
-static NEXT_COORDS: Lazy<Mutex<Vec<Vec<i64>>>> = Lazy::new(|| {
-    Mutex::new(vec![vec![]; TILE_NUM as usize])
-});
+
+fn initialize_global_info() -> Vec<Vec<usize>> {
+    const MOVE: [(usize, usize); 4] = [(!0, 0), (0, !0), (1, 0), (0, 1)];
+    let mut next_coords = vec![vec![]; TILE_NUM];
+    eprint!("initialize global info\n");
+
+    for y in 0..H {
+        for x in 0..W {
+            let coord = y * W + x;
+            for &(dy, dx) in &MOVE {
+                let ty = y.wrapping_add(dy);
+                let tx = x.wrapping_add(dx);
+                if (0..H).contains(&ty) && (0..W).contains(&tx) {
+                    let tcoord = ty * W + tx;
+                    if TILES[coord] != TILES[tcoord] {
+                        next_coords[coord].push(tcoord);
+                    }
+                }
+            }
+        }
+    }
+    next_coords
+}
 
 struct State {
-    path: Vec<i64>
+    path: Vec<usize>
 }
 
 impl State {
     fn output(&self) -> String {
         let mut res = String::new();
         for i in 0..self.path.len()-1 {
-            let diff = self.path[i+1] - self.path[i];
-            match diff {
-                1 => res.push('R'),
-                -1 => res.push('L'),
-                d if d == W => res.push('D'),
-                d if d == -W => res.push('U'),
-                _ => panic!("Error!"),
-            }
+            if self.path[i+1] == self.path[i] + 1 {
+                res.push('R');
+            } else if self.path[i+1] + 1 == self.path[i] {
+                res.push('L');
+            } else if self.path[i+1] == self.path[i] + W {
+                res.push('D');
+            } else if self.path[i+1] + W == self.path[i] {
+                res.push('U');
+            } 
         }
         res
-    }
-}
-
-fn initialize_global_info() {
-    const DY: [i64; 4] = [-1, 1, 0, 0];
-    const DX: [i64; 4] = [0, 0, 1, -1];
-    for y in 0..H {
-        for x in 0..W {
-            let coord = y * W + x;
-            for d in 0..4usize {
-                let ty = y + DY[d];
-                let tx = x + DX[d];
-                if (0..H).contains(&ty) && (0..W).contains(&tx) {
-                    let tcoord = ty * W + tx;
-                    if TILES[coord as usize] != TILES[tcoord as usize] {
-                        NEXT_COORDS.lock().unwrap()[coord as usize].push(tcoord);
-                    }
-                }
-            }
-        }
     }
 }
 
@@ -83,6 +85,7 @@ fn main() {
 
     lazy_static::initialize(&TILES);
     lazy_static::initialize(&POINTS);
+    lazy_static::initialize(&NEXT_COORDS);
 
     println!("{} {}", TILES[0], POINTS[0]);
 }
